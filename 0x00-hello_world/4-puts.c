732a7c96cf054607e550c5a14e4b9d302345d405
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-08 18:24:30 -0700

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char sentence[] = "Programming is like building a multilingual puzzle";
+	char sentence[] = "\"Programming is like building a multilingual puzzle";
 	puts(sentence);
 	return 0;
 }
