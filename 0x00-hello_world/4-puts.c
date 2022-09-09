563cd5e592886aa5248e8cbbe6c2a89f937fd607
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-08 18:21:37 -0700

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+/**
+ *
+ * *Description: a C program that prints exactly "Programming is like building a multilingual puzzle
+ *
+ *
+ *     * Return: 0 if executed properly
+ *
+ * */
+
 int main(void)
-	{
-char sentence[] = "Programming is like building a multilingual puzzle";
-puts(sentence);
-return 0;
-	}
+{
+	char sentence[] = "Programming is like building a multilingual puzzle";
+	puts(sentence);
+	return 0;
+}
 
