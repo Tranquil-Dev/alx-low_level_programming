cc42b3ad92c11386362c9ee098aa2806997febcf
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-08 18:31:09 -0700

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,9 @@
 
 /**
  *
- * *Description: a C program that prints exactly "Programming is like building a multilingual puzzle
- *
- *
- *     * Return: 0 if executed properly
- *
- * */
+ * main: a C program that prints exactly "Programming is like building a multilingual puzzle
+ * return: 0 if executed properly
+ */
 
 int main(void)
 {
