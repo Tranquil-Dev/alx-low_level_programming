ecd5846377ff65bf0193a845cce08f814623d465
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-08 19:15:36 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints a string using printf
+ *
+ * Return:  0 if execution was successful
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
