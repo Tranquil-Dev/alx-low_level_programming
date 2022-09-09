3ffde62e8d29f30912ecbf7fc0716c5414d923a9
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-08 20:17:44 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include<unistd.h>
+
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * 
- * Return: 0 (success)
+ * main - Entry point
+ *
+ * Description: prints a string without using printf or puts functions
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char Z[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, Z, 59);
+	char M[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, M, 59);
 	return (1);
 }
