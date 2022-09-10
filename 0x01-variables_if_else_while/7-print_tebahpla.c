cb3a050e68f732d86084a6adc9d5dc616b774551
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-09 20:30:21 -0700

    Update 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints out the alphabets in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha <= 'a'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
