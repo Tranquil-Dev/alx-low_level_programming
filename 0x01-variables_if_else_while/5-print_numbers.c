8b8e9b55b77b711527d77e8e9105960f000540e0
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-09 20:04:08 -0700

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - it prints all single digits from 0-9
+ * main - A program that will print all sigle digit numbers
  *
- * Return: 0
+ * Return: always 0
  */
 int main(void)
 {
@@ -12,7 +11,6 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		printf("%d", num);
-		num++;
 	}
 	printf("\n");
 	return (0);
