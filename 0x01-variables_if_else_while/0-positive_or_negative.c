5c59d00a8d0952c9b5b024b05db8b87bf3f7f81f
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-09 17:13:48 -0700

    added 0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -entry point 
+ *
+ * Description- a code to print whether a variable stored is positive or negativ
+ *
+ * Result - 0
+*/
+int main(void)
+{
+	
+}
