00acd6ed2224ee9a247c918c4f0fb69d2d09e515
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-09 19:36:15 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-  char alpha;
+	char alpha;
   
-  for (alpha = 'a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
 	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 		putchar(alpha);
