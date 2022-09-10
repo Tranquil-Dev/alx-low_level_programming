e066d879c7df25fbbcadc3d2944692cc5e5fabe4
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-09 20:59:13 -0700

    Update 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimals
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; n < 17; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
