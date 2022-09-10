0852651b52dd2c022d86a35f64cbcf05a3a58b31
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-09 20:21:27 -0700

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digits 0-9
+ * main - prints all digits
  *
- * Return: 0
+ * Return: 0 always (Success)
  */
 int main(void)
 {
- int a;
- for (a = 0; a < 10; a++)
- {
-  printf("%d", a);
- }
- printf("\n");
- return (0);
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+	return (0);
 }
