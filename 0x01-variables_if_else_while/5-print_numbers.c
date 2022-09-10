5a079dbbad2a82c1a206a97d9a959ad00aa62fc4
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-09 20:14:36 -0700

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,15 @@
  * main - prints all single digit between 0-9
  *
  * Return - 0
-*/
+ */
 int main(void)
 {
   int a;
   
   for (a = 0; a < 10; a++)
+  {
     printf("%d", a);
+  }
   printf("\n");
   return (0);
 }
