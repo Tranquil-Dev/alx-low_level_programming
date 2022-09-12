e63751fb41a851a0d7062a7662be2f31fb3ba9a8
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-12 06:52:54 -0700

    added 8-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,12 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num != '9') 
+		if (num != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	
 	putchar('\n');
 	return (0);
 }
