493a93f8ceb804278398fa316ceaa52324151646
Captain Adamson <abdulqudusadamson@gmail.com>
2022-09-13 18:05:51 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the alphabet x10
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * Return: void.
+ * Return: Always 0
  */
 void print_alphabet_x10(void)
 {
