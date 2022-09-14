2dc2c0d7bd6babafc5ae06a8836bde27afe99832
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-13 17:57:20 -0700

    updated 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the alphabet x10
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	char alpnum;
+	int num;
+
+	for (num = 1; nu < 11; num++)
+	{
+		for (alpnum = 'a'; alpnum <= 'z'; alpnum++)
+		{
+			_putchar(alpnum);
+		}
+	}
+	_putchar('\n');
+}
