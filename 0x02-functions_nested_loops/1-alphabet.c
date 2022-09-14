d06c40da207accbe73266606e8d622f6bbbdb43b
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-13 17:29:21 -0700

    added 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  main
+ *
+ *  return
+ */
+void print_alphabet(void)
+{
+	char alpnum;
+
+	for (alpnum = 'a'; alpnum <= 'z'; alpnum++)
+	{
+		_putchar(alpnum);
+	}
+	_putchar('\n');
+}
