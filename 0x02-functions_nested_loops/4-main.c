b978ca30b7c5e02133e2a42025c976637bfd813e
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-14 03:51:28 -0700

    added 4-main.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * _isalpha - A program that Shows 1 if the input is c
- * letter Another cases, shows 0
  *
- * @c: The Charater is ASCII code
  *
- * Return: 1 for for letters. Returns 0 otherwise
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('n');
+	return (0);
 }
