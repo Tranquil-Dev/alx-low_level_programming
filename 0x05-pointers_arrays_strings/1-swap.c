012b5336b5ac375c0fcf77bed44bc9c667772980
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-19 09:50:13 -0700

    added 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  *  void swap_int(int *a, int *b) - swap integers
- *  @*a : function parameter is a integer
- *  @*b : function parameter is a integer
+ *  * @a : function parameter is a integer
+ *  * @b : function parameter is a integer
  *  Return : none
  */
 void swap_int(int *a, int *b)
