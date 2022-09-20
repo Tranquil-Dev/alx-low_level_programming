1d22d273b86af3025ef069fede38894eeb818fee
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-19 19:19:42 -0700

    updated 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s : parameter of _strlen function
+ * Return : length of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
