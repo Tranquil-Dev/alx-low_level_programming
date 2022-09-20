90ee900a40202224267f64af894c431458027a84
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-20 03:00:13 -0700

    added 5-101

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 /**
  * main - check the code
  *
@@ -9,6 +9,6 @@ int main(void)
     char *str;
 
     str = "0123456789";
-    puts2(str);
+    puts_half(str);
     return (0);
 }
