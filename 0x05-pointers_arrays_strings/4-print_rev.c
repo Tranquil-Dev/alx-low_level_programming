dd8fb182f47cbc274591507ff05472dafc16cf26
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-19 19:44:53 -0700

    updated 4-print_lev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s : parameter
+ *
+ * Return : string in reverse form
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	int i = 0;
+
+
+
+		while (s[i] != '\0')
+
+					i++;
+
+
+
+			while (i > 0)
+
+					{
+
+								_putchar(s[i - 1]);
+
+										i--;
+
+											}
+
+				_putchar('\n');
+
+				:u
+
+
+}
